f45dbb06f6a3385c4b2ce4032d1e6cf8594cc48d
kana0011 <kana.incogito@gmail.com>
2017-10-14 15:59:40 +0800

    Create save account modal
    Signed-off-by: kana0011 <kana.incogito@gmail.com>

--- ph.kana.memory.ui/src/ph/kana/memory/ui/MainFormController.java
+++ ph.kana.memory.ui/src/ph/kana/memory/ui/MainFormController.java
@@ -1,6 +1,7 @@
 package ph.kana.memory.ui;
 
 import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -25,9 +26,44 @@ public class MainFormController implements Initializable {
 	@FXML
 	private Pane viewPane;
 
+	@FXML
+	private Pane saveAccountPane;
+
+	@FXML
+	private Label saveAccountPaneTitle;
+
+	@FXML
+	private TextField domainTextBox;
+
+	@FXML
+	private TextField usernameTextBox;
+
+	@FXML
+	private TextField unmaskedPasswordTextBox;
+
+	@FXML
+	private PasswordField maskedPasswordTextBox;
+
+	@FXML
+	private CheckBox maskPasswordToggle;
+
+	@FXML
+	public void showAddAccountDialog() {
+		saveAccountPaneTitle.setText("Add Account");
+		saveAccountPane.setVisible(true);
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		Platform.runLater(this::bindPasswordToggle);
 		Platform.runLater(this::loadAccounts);
+	}
+
+	private void bindPasswordToggle() {
+		unmaskedPasswordTextBox.textProperty()
+				.bindBidirectional(maskedPasswordTextBox.textProperty());
+		unmaskedPasswordTextBox.visibleProperty()
+				.bind(maskPasswordToggle.selectedProperty().not());
 	}
 
 	private void loadAccounts() {
@@ -65,7 +101,7 @@ public class MainFormController implements Initializable {
 
 		Button showButton = new Button("Show");
 		children.add(showButton);
-		showButton.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
+		showButton.setOnAction(event -> {
 			String encryptedPassword = account.getEncryptedPassword();
 			// TODO: do something with password
 		});
@@ -76,12 +112,12 @@ public class MainFormController implements Initializable {
 		List<MenuItem> menuItems = accountMenu.getItems();
 		MenuItem updateMenuItem = new MenuItem("Update");
 		menuItems.add(updateMenuItem);
-		updateMenuItem.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+		updateMenuItem.setOnAction(event -> {
 			// TODO: update account
 		});
 		MenuItem deleteMenuItem = new MenuItem("Delete");
 		menuItems.add(deleteMenuItem);
-		deleteMenuItem.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+		deleteMenuItem.setOnAction(event -> {
 			// TODO: delete account
 		});
 		assignAnchors(showButton, null, 10.0, 5.0, null);
