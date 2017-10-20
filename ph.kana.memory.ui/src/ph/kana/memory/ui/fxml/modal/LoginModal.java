3aae64c76c66aa0bd41869fe2de50280f5c77077
kana0011 <kana.incogito@gmail.com>
2017-10-21 04:36:22 +0800

    Utilize fade text utility method
    Signed-off-by: kana0011 <kana.incogito@gmail.com>

--- ph.kana.memory.ui/src/ph/kana/memory/ui/fxml/modal/LoginModal.java
+++ ph.kana.memory.ui/src/ph/kana/memory/ui/fxml/modal/LoginModal.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import ph.kana.memory.stash.AuthService;
 import ph.kana.memory.stash.StashException;
+import ph.kana.memory.ui.fxml.UiCommons;
 
 public class LoginModal extends AbstractTilePaneModal {
 
@@ -29,7 +30,7 @@ public class LoginModal extends AbstractTilePaneModal {
 				pinTextBox.setText("");
 			}
 		} catch (StashException e) {
-//			showBottomMessage("Cannot validate PIN"); // TODO: wait for it to be externalized
+			UiCommons.showBottomFadingText("Cannot validate PIN", getChildren());
 			e.printStackTrace(System.err);
 		}
 	}
