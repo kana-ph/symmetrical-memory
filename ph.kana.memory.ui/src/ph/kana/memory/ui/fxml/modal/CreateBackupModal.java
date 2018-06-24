0195b70a857ee69e2c7a03219dea29d8caf826f2
kana0011 <kana.incognito@gmail.com>
2018-06-25 04:52:30 +0800

    Disable backup/restore features
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- ph.kana.memory.ui/src/ph/kana/memory/ui/fxml/modal/CreateBackupModal.java
+++ ph.kana.memory.ui/src/ph/kana/memory/ui/fxml/modal/CreateBackupModal.java
@@ -2,12 +2,15 @@ package ph.kana.memory.ui.fxml.modal;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 import java.util.Objects;
 
 import static javafx.stage.FileChooser.ExtensionFilter;
@@ -16,12 +19,17 @@ public class CreateBackupModal extends AbstractTilePaneModal<Void, Void> {
 
 	@FXML private Button saveButton;
 	@FXML private TextField fileTextBox;
+	@FXML private TextField unmaskedPasswordTextBox;
+	@FXML private PasswordField maskedPasswordTextBox;
+	@FXML private CheckBox maskPasswordToggle;
 	private File backupFile = null;
 
 	private static ExtensionFilter FILE_FILTER = new ExtensionFilter("Backup File (*.bak)","*.bak");
 
 	public CreateBackupModal() {
 		super("create-backup-modal.fxml");
+
+		bindPasswordToggle();
 	}
 
 	@FXML
@@ -40,13 +48,20 @@ public class CreateBackupModal extends AbstractTilePaneModal<Void, Void> {
 			var filename = backupFile.getAbsolutePath();
 			fileTextBox.setText(filename);
 			fileTextBox.positionCaret(filename.length());
-
-			saveButton.setDisable(false);
 		}
 	}
 
 	@FXML
 	public void createBackup() {
+		var fieldValidateMap = Map.of(
+				fileTextBox, "Please select backup file.",
+				maskedPasswordTextBox, "Backup password is required!"
+		);
+		var formValid = validateFields(fieldValidateMap);
+		if (!formValid) {
+			return;
+		}
+
 		showBottomFadingText("Successfully created backup:\n" + backupFile.getAbsolutePath());
 		close();
 	}
@@ -54,6 +69,16 @@ public class CreateBackupModal extends AbstractTilePaneModal<Void, Void> {
 	@Override
 	public void showModal(Void data) {
 		setVisible(true);
+	}
+
+	private void bindPasswordToggle() {
+		unmaskedPasswordTextBox.textProperty()
+				.bindBidirectional(maskedPasswordTextBox.textProperty());
+		unmaskedPasswordTextBox.translateXProperty()
+				.bindBidirectional((maskedPasswordTextBox.translateXProperty()));
+
+		maskedPasswordTextBox.visibleProperty()
+				.bind(maskPasswordToggle.selectedProperty().not());
 	}
 
 	private String generateInitialFileName() {
